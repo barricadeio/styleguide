2ae21a14ae8ea6dc14c2ed85bafe83b961275b3c
Ronan Flynn-Curran <flynnduism@gmail.com>
2015-04-03 12:34:06 +0100

    Initial commit

---
+++ app/styleguide/intro/intro.controller.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var app = angular.module('app');
+app.controller('StyleguideIntroCtrl', function ($scope, user) {
+  // variables
+  $scope.user = {};
+});
---
+++ app/styleguide/main/main.controller.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var app = angular.module('app');
+app.controller('StyleguideMainCtrl', function ($scope, user) {
+  // variables
+  $scope.user = {};
+});
