ef29e845444d41ea9740fd6217b5763d8e50f11f
Ronan Flynn-Curran <flynnduism@gmail.com>
2015-04-03 17:26:28 +0100

    Switch to a static styleguide site

--- app/styleguide/styles/styles.controller.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var app = angular.module('app');
-app.controller('StyleguideIntroCtrl', function ($scope, user) {
-  // variables
-  $scope.user = {};
-});
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+if (file_exists('index.html')) {
+  echo file_get_contents('index.html');
+}
